6bf123bea304bdd421e9ceda5869b1267e285176
quangmindx <j2team.tranminhquang@gmail.com>
2020-12-01 13:09:55 +0700

    fix forgot password

--- model/Fotgot_password.php
+++ model/Fotgot_password.php
@@ -5,11 +5,14 @@
         $subject = "Forgot Password";
         $body = "Xin chào, mật khẩu của bạn đã được đổi thành ". $new_password. " . Vui lòng đăng nhập bằng mật khẩu này cho lần đăng nhập tiếp theo";
         $headers = "From j2team.tranminhquang@gmail.com";
-        if(mail($to_email, $subject, $body, $headers)) {
-            $query = "UPDATE user SET password = md5('$new_password') WHERE email = '$email';";
-            $conn->query($query);
-            return "Email sending successfully";
+        if(empty($to_email)) {
+            return "Email are required!";
+        } else if(mail($to_email, $subject, $body, $headers)) {
+                $query = "UPDATE user SET password = md5('$new_password') WHERE email = '$email';";
+                $conn->query($query);
+                return "Email sending successfully";
+        } else {
+            return "Email sending faild";
         }
-        return "Email sending faild";
     }
 ?>
